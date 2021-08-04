1a0a7b4fc8c0e76c75a4784ca6df586cae772468
Breder <lucasbreder@gmail.com>
2021-08-04 08:52:01 -0300

    Versão 0.2

---
+++ next.config.js
@@ -0,0 +1,64 @@
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /plugin\.css$/,
+        loaders: ['style-loader', 'css'],
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_NAME: "Patrimonio",
+    NEXT_PUBLIC_LANGUAGE: 'pt-br',
+    NEXT_BASE_PATH: "https://patrimonio-app.vercel.app/",
+    NEXT_PUBLIC_API: "https://patrimonio-321811.uc.r.appspot.com/",
+    NEXT_PUBLIC_NAME_STORAGEBASEURL: "https://storage.googleapis.com/patrimonio-321811.appspot.com/",
+    NEXT_PUBLIC_EXCLUDEFROMLIST: ['id', 'content', 'project', 'text', 'path', 'file', 'speech', 'remember_me_token', 'testimony', 'user_id', 'update_user_id', 'created_at', 'updated_at', 'baseMaterial', 'serial_number', 'management', 'subitem', 'price'],
+    NEXT_PUBLIC_FOREIGNKEYLIST: ['update_user_id', 'user_id', 'unit_id', 'category_id', 'local_id', 'sublocal_id', 'destination_user_id', 'destination_unit_id', 'material_id', 'base_material_id'],
+    NEXT_PUBLIC_BOOLEANLIST: ['active', 'feature', 'terms'],
+    NEXT_PUBLIC_ROUTES: [
+      {
+        title: "Materiais Base",
+        path: "/base_materials",
+        icon: "box"
+      },
+      {
+        title: "Categorias",
+        path: "/categories",
+        icon: "tag"
+      },
+      {
+        title: "Materiais da Unidade",
+        path: "/materials",
+        icon: "unit"
+      },
+      {
+        title: "Locais da Unidade",
+        path: "/locals",
+        icon: "door"
+      },
+      {
+        title: "SubLocais da Unidade",
+        path: "/sublocals",
+        icon: "tools"
+      },
+      {
+        title: "Cautelas",
+        path: "/loans",
+        icon: "doc"
+      },
+      {
+        title: "Usuarios",
+        path: "/users",
+        icon: "users"
+      },
+      {
+        title: "Checagem",
+        path: "/check",
+        icon: "check"
+      },
+
+    ]
+  },
+    
+}
